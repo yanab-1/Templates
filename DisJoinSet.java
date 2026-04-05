63e9cc0a9da71b915ebdd131a0e9063d689c7a31
Yash Bansal <157395996+yanab-1@users.noreply.github.com>
2026-04-06 02:55:30 +0530

    Refactor DisJoinSet methods for clarity and efficiency
    Updated rank initialization and path compression logic.

--- DisJoinSet.java
+++ DisJoinSet.java
@@ -1,55 +1,56 @@
 public class DisJoinSet {
-	int[] parent, size, rank;
+    int[] parent, size, rank;
 
-	public DisJoinSet(int n){
-		parent = new int[n];
-		size = new int[n];
-		rank = new int[n];
-		for(int i = 0; i < n; i++) create(i);
-	}
+    public DisJoinSet(int n){
+        parent = new int[n];
+        size = new int[n];
+        rank = new int[n];
+        for(int i = 0; i < n; i++) create(i);
+    }
 
-	public void create(int v) {
-		parent[v] = v;
-		size[v] = 1;
-		rank[v] = 1;
-	}
+    public void create(int v) {
+        parent[v] = v;
+        size[v] = 1;
+        rank[v] = 0; // better
+    }
 
-	public int find(int node) {
-		if (parent[node] == node) {
-			return node;
-		}
-		int n = find(parent[node]);
-		parent[node] = n;// path Compression
-		return n;
+    public int find(int node) {
+        if (parent[node] != node) {
+            parent[node] = find(parent[node]); // path compression
+        }
+        return parent[node];
+    }
 
-	}
+    public void unionByRank(int v1, int v2) {
+        int re1 = find(v1);
+        int re2 = find(v2);
 
-	public void unionByRank(int v1, int v2) {
-		int re1 = find(v1);// v1 ka re node hai
-		int re2 = find(v2);// v2 ka re node hai
-		if(parent[re1] == parent[re2]) return;
-		else if (rank[re1] == rank[re2]){
-			parent[re1] = re2;
-			rank[re2]++;
-		}
-		else if (rank[re1] < rank[re2]) {
-			parent[re1] = re2;
-		} 
-		else {
-			parent[re2] = re1;
-		}
-	}
+        if(re1 == re2) return;
 
-	public void unionBySize(int v1, int v2) {
-		int re1 = find(v1);// v1 ka re node hai
-		int re2 = find(v2);// v2 ka re node hai
-		if(parent[re1] == parent[re2]) return;
-		else if (size[re1] < size[re2]) {
-			parent[re1] = re2;
-			size[re2] += size[re1];
-		} else {
-			parent[re2] = re1;
-			size[re1] += size[re2];
-		}
-	}
+        if (rank[re1] < rank[re2]) {
+            parent[re1] = re2;
+        } 
+        else if (rank[re1] > rank[re2]) {
+            parent[re2] = re1;
+        } 
+        else {
+            parent[re2] = re1;
+            rank[re1]++;
+        }
+    }
+
+    public void unionBySize(int v1, int v2) {
+        int re1 = find(v1);
+        int re2 = find(v2);
+
+        if(re1 == re2) return;
+
+        if (size[re1] < size[re2]) {
+            parent[re1] = re2;
+            size[re2] += size[re1];
+        } else {
+            parent[re2] = re1;
+            size[re1] += size[re2];
+        }
+    }
 }
